24c14737e87aa9d2e6a82f8ff8fb40f8aab1bc7d
IjustLostMyDog869 <casiano.j@stu.janesville.k12.wi.us>
2024-11-01 09:32:05 -0500

    11/1 twix

--- lang152a/lang152a/MainForm.py
+++ lang152a/lang152a/MainForm.py
@@ -5,86 +5,92 @@ from System.Drawing import *
 from System.Windows.Forms import *
 
 class MainForm(Form):
-	def __init__(self):
-		self.InitializeComponent()
-	
-	def InitializeComponent(self):
-		self._listBox1 = System.Windows.Forms.ListBox()
-		self._button1 = System.Windows.Forms.Button()
-		self._button2 = System.Windows.Forms.Button()
-		self._button3 = System.Windows.Forms.Button()
-		self.SuspendLayout()
-		# 
-		# listBox1
-		# 
-		self._listBox1.BackColor = System.Drawing.Color.PaleGoldenrod
-		self._listBox1.FormattingEnabled = True
-		self._listBox1.ItemHeight = 42
-		self._listBox1.Location = System.Drawing.Point(12, 12)
-		self._listBox1.Name = "listBox1"
-		self._listBox1.Size = System.Drawing.Size(212, 298)
-		self._listBox1.TabIndex = 0
-		# 
-		# button1
-		# 
-		self._button1.BackColor = System.Drawing.Color.Aqua
-		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._button1.Location = System.Drawing.Point(247, 22)
-		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(140, 88)
-		self._button1.TabIndex = 1
-		self._button1.Text = "calculate"
-		self._button1.UseVisualStyleBackColor = False
-		self._button1.Click += self.Button1Click
-		# 
-		# button2
-		# 
-		self._button2.BackColor = System.Drawing.Color.Gold
-		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._button2.Location = System.Drawing.Point(247, 129)
-		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(140, 88)
-		self._button2.TabIndex = 2
-		self._button2.Text = "clear"
-		self._button2.UseVisualStyleBackColor = False
-		self._button2.Click += self.Button2Click
-		# 
-		# button3
-		# 
-		self._button3.BackColor = System.Drawing.Color.GreenYellow
-		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 20.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._button3.Location = System.Drawing.Point(247, 223)
-		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(140, 88)
-		self._button3.TabIndex = 3
-		self._button3.Text = "exit"
-		self._button3.UseVisualStyleBackColor = False
-		self._button3.Click += self.Button3Click
-		# 
-		# MainForm
-		# 
-		self.BackColor = System.Drawing.Color.Silver
-		self.ClientSize = System.Drawing.Size(410, 322)
-		self.Controls.Add(self._button3)
-		self.Controls.Add(self._button2)
-		self.Controls.Add(self._button1)
-		self.Controls.Add(self._listBox1)
-		self.Font = System.Drawing.Font("Microsoft Sans Serif", 27.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self.Name = "MainForm"
-		self.Text = "pro152a"
-		self.ResumeLayout(False)
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+					self._listBox1 = System.Windows.Forms.ListBox()
+					self._button1 = System.Windows.Forms.Button()
+					self._button2 = System.Windows.Forms.Button()
+					self._button3 = System.Windows.Forms.Button()
+					self.SuspendLayout()
+					# 
+					# listBox1
+					# 
+					self._listBox1.Font = System.Drawing.Font("Nirmala UI", 8.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+					self._listBox1.ForeColor = System.Drawing.Color.Navy
+					self._listBox1.FormattingEnabled = True
+					self._listBox1.Location = System.Drawing.Point(2, 12)
+					self._listBox1.Name = "listBox1"
+					self._listBox1.Size = System.Drawing.Size(373, 290)
+					self._listBox1.TabIndex = 0
+					# 
+					# button1
+					# 
+					self._button1.BackColor = System.Drawing.Color.Aquamarine
+					self._button1.Font = System.Drawing.Font("Palatino Linotype", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+					self._button1.Location = System.Drawing.Point(381, 24)
+					self._button1.Name = "button1"
+					self._button1.Size = System.Drawing.Size(109, 49)
+					self._button1.TabIndex = 1
+					self._button1.Text = "Calculate"
+					self._button1.UseVisualStyleBackColor = False
+					self._button1.Click += self.Button1Click
+					# 
+					# button2
+					# 
+					self._button2.BackColor = System.Drawing.Color.Aquamarine
+					self._button2.Font = System.Drawing.Font("Palatino Linotype", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+					self._button2.Location = System.Drawing.Point(381, 132)
+					self._button2.Name = "button2"
+					self._button2.Size = System.Drawing.Size(109, 49)
+					self._button2.TabIndex = 2
+					self._button2.Text = "Clear"
+					self._button2.UseVisualStyleBackColor = False
+					self._button2.Click += self.Button2Click
+					# 
+					# button3
+					# 
+					self._button3.BackColor = System.Drawing.Color.Aquamarine
+					self._button3.Font = System.Drawing.Font("Palatino Linotype", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+					self._button3.Location = System.Drawing.Point(381, 245)
+					self._button3.Name = "button3"
+					self._button3.Size = System.Drawing.Size(109, 49)
+					self._button3.TabIndex = 3
+					self._button3.Text = "Exit"
+					self._button3.UseVisualStyleBackColor = False
+					self._button3.Click += self.Button3Click
+					# 
+					# MainForm
+					# 
+					self.BackColor = System.Drawing.Color.FromArgb(0, 192, 192)
+					self.ClientSize = System.Drawing.Size(497, 328)
+					self.Controls.Add(self._button3)
+					self.Controls.Add(self._button2)
+					self.Controls.Add(self._button1)
+					self.Controls.Add(self._listBox1)
+					self.Name = "MainForm"
+					self.Text = "Prog152a"
+					self.ResumeLayout(False)
 
 
-	def Button3Click(self, sender, e):
-		Application.Exit()
+    def Button1Click(self, sender, e):
+        header = "Sum :  "
+        self._listBox1.Items.Add(header)
+        sum = 0
+        for num in range (3,9669+1,3):
+            sum = sum + num 
+        self._listBox1.Items.Add(sum)
+        
+        list = "3 + 6 + 9 + 12 + 15 + 18 + 21 + ...+ 24 + 9663 + 9666 + 9669 = " + str(sum)
+        self._listBox1.Items.Add(list)
 
-	def Button2Click(self, sender, e):
-		self._listBox1.Items.Clear()
+        
+    def Button2Click(self, sender, e):
+        self._listBox1.Items.Clear()
 
-	def Button1Click(self, sender, e):
-		Sum = 15586428
-		answer = "\t\t" + str(Sum)
-		self._listBox1.Items.Add(answer)
-		for num in range(3, 9670, 3):
-			line = str(num)
-			self._listBox1.Items.Add(line) 
+    def Button3Click(self, sender, e):
+        Application.Exit()
+
+    def Label1Click(self, sender, e):
+        pass
