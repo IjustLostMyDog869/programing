334d58ca86b039ab479f8c417429df6965a1d22a
IjustLostMyDog869 <casiano.j@stu.janesville.k12.wi.us>
2024-11-06 09:07:00 -0600

    are we cooked chat??? 11/6/24

---
+++ String from minecraft/Program.py
@@ -0,0 +1,13 @@
+﻿bling = "DOUBLE chunk CHOCALATE cookie IS now HERE in THE world"
+print(bling.lower())
+print(bling.upper())
+print(bling.capitalize())
+print(bling.title())
+print(bling.isdigit())
+print(bling.find("world"))
+print(bling.count("E"))
+print(bling[::-1])
+print(bling.split(" "))
+print("CHOCALATE!" * 5)
+
+input()
